435efe7df40266f01687074f0daa949cfd20f757
Jianhui Yuan Xu <neuromoments@gmail.com>
2023-04-17 16:20:52 +0800

    Good luck! Bye~

--- CPU_ComOrg.srcs/sources_1/new/ALU.v
+++ CPU_ComOrg.srcs/sources_1/new/ALU.v
@@ -21,6 +21,29 @@
 
 
 module ALU (
-
+  dina,     // data input a
+  dinb,     // data input b
+  aluc,     // ALU control
+  r,        // return value
+  z         // zero
 );
+  input[31:0] dina, dinb;
+  input[3:0] aluc;
+  output[31:0] r;
+  output z;
+  
+  wire[32:0] r_add = dina + dinb;   // add, addi
+  wire[32:0] r_minus = dina - dinb; // sub, subi, beq, bnq
+  wire[31:0] r_and = dina & dinb;   // and
+  wire[31:0] r_or = dina | dinb;    // or
+  wire[31:0] r_xor = dina ^ dinb;   // xor
+  wire[31:0] r_lui = {dinb[15:0], 16'b0};
+  
+  always @(*) begin
+    case (aluc)
+      
+    endcase
+  end
+
+
 endmodule
--- CPU_ComOrg.srcs/sources_1/new/Controller.v
+++ CPU_ComOrg.srcs/sources_1/new/Controller.v
@@ -21,6 +21,24 @@
 
 
 module Controller(
-
+  op,       // op from IFetch
+  func,     // func from IFetch
+  z,        // zero from ALU, +ve if zero
+  pcsource, // mulplex to choose pc. 
+            // 0: PC+4; 1: PC relative; 2: Base; 3: Pseudo
+  aluc,     // alu control
+  wreg,     // enable to write register
+  wmem,     // write memory
+  jal,      // if jump and link
   );
+
+  input [5:0] op, func;
+  input z;
+  output[3:0] aluc;
+  output[1:0] pcsource;
+  output wreg, wmem, jal;
+  
+  
+
+
 endmodule
